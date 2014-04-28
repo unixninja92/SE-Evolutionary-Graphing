62c382c4528a1fb1125402c8ad1bcd8cca793add
unixninja92 <charlesvt@gmail.com>
2014-04-27 23:08:18 -0400

    basic file name saving

--- parsing_gui.py
+++ parsing_gui.py
@@ -1,5 +1,8 @@
 import gtk
 
+runs = [[]]
+
+
 class DataParsingGUI:
 	
     def delete_event(self, widget, event, data=None):
@@ -18,9 +21,25 @@ class DataParsingGUI:
         self.baseVBox = gtk.VBox()
         self.window.add(self.baseVBox)
         self.baseVBox.show()
-        
+
+        self.runInteraction = gtk.HBox()
+        self.baseVBox.add(self.runInteraction)
+        self.runInteraction.show()
+
+        self.addButton = gtk.Button("Add File")
+        self.addButton.connect("clicked", self.addFiles, 1)
+        self.runInteraction.add(self.addButton)
+        self.addButton.show()
 
         self.window.show()
+
+    def addFiles(self, widget, runNum):
+        self.fileSelect = gtk.FileChooserDialog(title = "", action = gtk.FILE_CHOOSER_ACTION_OPEN, buttons = (gtk.STOCK_CANCEL, gtk.RESPONSE_CANCEL, gtk.STOCK_OPEN, gtk.RESPONSE_OK))
+        self.fileSelect.set_select_multiple(True)
+        
+        self.files = self.fileSelect.run()
+        runs[runNum].append(self.fileSelect.get_filenames())
+        self.fileSelect.destroy()
 
     def main(self):
         # All PyGTK applications must have a gtk.main(). Control ends here
@@ -28,12 +47,18 @@ class DataParsingGUI:
         gtk.main()
 
 class runBox:
-    def __init__:
+    def __init__(self, run):
         self.base = gtk.HBox()
+        self.runName = gtk.Label(run)
         
-
+        self.base.add(self.runName)
+        
+        self.runName.show()
         self.base.show()
 
 if __name__ == "__main__":
     hello = DataParsingGUI()
     hello.main()
+
+
+#Have list of currently selected files for each run, and allow user to add folders or files to the list. Then allow user to add more runs.
