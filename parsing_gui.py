d9f33369e4d160aca2671c0aa61bf066c37362fc
unixninja92 <charlesvt@gmail.com>
2014-04-27 21:44:04 -0400

    init data parsing gui

---
+++ parsing_gui.py
@@ -0,0 +1,39 @@
+import gtk
+
+class DataParsingGUI:
+	
+    def delete_event(self, widget, event, data=None):
+        return False
+
+    def destroy(self, widget, data=None):
+        gtk.main_quit()
+
+    def __init__(self):
+        self.window = gtk.Window(gtk.WINDOW_TOPLEVEL)
+
+        self.window.connect("delete_event", self.delete_event)
+        self.window.connect("destroy", self.destroy)
+        self.window.set_border_width(10)
+
+        self.baseVBox = gtk.VBox()
+        self.window.add(self.baseVBox)
+        self.baseVBox.show()
+        
+
+        self.window.show()
+
+    def main(self):
+        # All PyGTK applications must have a gtk.main(). Control ends here
+        # and waits for an event to occur (like a key press or mouse event).
+        gtk.main()
+
+class runBox:
+    def __init__:
+        self.base = gtk.HBox()
+        
+
+        self.base.show()
+
+if __name__ == "__main__":
+    hello = DataParsingGUI()
+    hello.main()
