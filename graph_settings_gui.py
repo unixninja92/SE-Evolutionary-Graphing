ccdfcce13f76cfad3f863285e25427ba8bb89154
Bmanlinehan <bmanlinehan@gmail.com>
2014-05-07 18:47:44 -0400

    Update graph_settings_gui.py

--- graph_settings_gui.py
+++ graph_settings_gui.py
@@ -157,6 +157,6 @@ class GraphSettingsGUI:
 
 # If the program is run directly or passed as an argument to the python
 # interpreter then create a HelloWorld instance and show it
-#if __name__ == "__main__":
-    #hello = GraphSettingsGUI()
-    #hello.main()
+if __name__ == "__main__":
+    hello = GraphSettingsGUI()
+    hello.main()
