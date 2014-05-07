21bdfbcb39b19fae28984f55ff501dcefce24914
unixninja92 <charlesvt@gmail.com>
2014-05-07 02:23:55 -0400

    added fields for what data to extract for parsing data gui. Also moved general gui elements to their own class so they can be shared between guis.

--- parsing_gui.py
+++ parsing_gui.py
@@ -1,6 +1,25 @@
 import gtk
+import data_parser
+import gui_components
 
 sets = []
+
+#compute gnetic difference and/or phenotipic difference
+#if L-systems (only for gentic)
+#   - num terminals
+#   - num non-terminals
+#   - expansion rate
+#ask for filname (autogen one first)
+#ask for first generation(index) to use
+
+
+#Finds:
+#   -bests 
+#   -averages
+#   -XValues
+#   -deversities 
+#   -gnetic diversity 
+#   -phenotipic diversity 
 
 
 class DataParsingGUI:
@@ -12,6 +31,8 @@ class DataParsingGUI:
         gtk.main_quit()
 
     def __init__(self):
+        self.dataToParse = data_parser.DataParser()
+
         self.window = gtk.Window(gtk.WINDOW_TOPLEVEL)
 
         self.window.connect("delete_event", self.delete_event)
@@ -27,7 +48,7 @@ class DataParsingGUI:
         self.scrollSets = gtk.ScrolledWindow()
         # self.scrollSets.set_size_request(800,250)
         self.scrollSets.set_policy(gtk.POLICY_AUTOMATIC, gtk.POLICY_NEVER)
-        self.baseVBox.pack_start(self.scrollSets, True, True, 0)
+        self.baseVBox.pack_start(self.scrollSets, True, True, 3)
         self.scrollSets.show()
 
         self.setHBox = gtk.HBox()
@@ -41,8 +62,10 @@ class DataParsingGUI:
 
         self.boxsets = []
 
+        #Add pre-parsed data file or add data sets
+
         self.setInteraction = gtk.HBox()
-        self.baseVBox.pack_start(self.setInteraction, False, False, 1)
+        self.baseVBox.pack_start(self.setInteraction, False, False, 5)
         self.setInteraction.show()
 
         self.parsedFrame = gtk.Frame("Parsed Data File")
@@ -64,8 +87,87 @@ class DataParsingGUI:
         self.setInteraction.pack_start(self.newSetButton, True, False, 1)
         self.newSetButton.show()
 
+        #Information about parsed data file that will be created when parse data is pressed
+
+        self.extractBox1 = gtk.HBox()
+        self.baseVBox.pack_start(self.extractBox1, False, False, 5)
+        self.extractBox1.show()
+
+        self.parsedFileLabel = gtk.Label("Staring Generation:")
+        self.extractBox1.pack_start(self.parsedFileLabel, False, False, 1)
+        self.parsedFileLabel.show()
+
+        self.startGenEntry = gui_components.NumericEntry()
+        self.startGenEntry.set_max_length(3)
+        self.startGenEntry.set_width_chars(3)
+        self.startGenEntry.set_text("1")
+        self.extractBox1.pack_start(self.startGenEntry, False, False, 1)
+        self.startGenEntry.show()
+
+        self.geneticDiff = gtk.CheckButton("Compute Genetic Differences")
+        self.geneticDiff.connect("toggled", self.isGenetic, None)
+        self.extractBox1.pack_start(self.geneticDiff, True, False, 0)
+        self.geneticDiff.show()
+
+        self.phenotypicDiff = gtk.CheckButton("Compute Phenotypic Differences")
+        self.extractBox1.pack_start(self.phenotypicDiff, True, False, 0)
+        self.phenotypicDiff.show()
+
+        self.parsedFileLabel = gtk.Label("File Name:")
+        self.extractBox1.pack_start(self.parsedFileLabel, False, False, 1)
+        self.parsedFileLabel.show()
+
+        self.parsedFileEntry = gtk.Entry()
+        self.extractBox1.pack_start(self.parsedFileEntry, False, False, 1)
+        self.parsedFileEntry.show()
+
+        #More info about new data file
+
+        self.extractBox2 = gtk.HBox()
+        self.baseVBox.pack_start(self.extractBox2, False, False, 5)
+        self.extractBox2.show()
+
+        self.lSystem = gtk.CheckButton("Is an L-System")
+        self.lSystem.connect("toggled", self.isLSystem, None)
+        self.lSystem.set_sensitive(False)
+        self.extractBox2.pack_start(self.lSystem, True, False, 2)
+        self.lSystem.show()
+
+        self.terminalLabel = gtk.Label("Number of Terminals:")
+        self.terminalLabel.set_sensitive(False)
+        self.extractBox2.pack_start(self.terminalLabel, False, False, 1)
+        self.terminalLabel.show()
+
+        self.terminalEntry = gui_components.NumericEntry()
+        self.terminalEntry.set_width_chars(4)
+        self.terminalEntry.set_sensitive(False)
+        self.extractBox2.pack_start(self.terminalEntry, False, False, 0)
+        self.terminalEntry.show()
+
+        self.nonterminalLabel = gtk.Label("Number of Non-Terminals:")
+        self.nonterminalLabel.set_sensitive(False)
+        self.extractBox2.pack_start(self.nonterminalLabel, False, False, 1)
+        self.nonterminalLabel.show()
+
+        self.nonterminalEntry = gui_components.NumericEntry()
+        self.nonterminalEntry.set_width_chars(4)
+        self.nonterminalEntry.set_sensitive(False)
+        self.extractBox2.pack_start(self.nonterminalEntry, False, False, 0)
+        self.nonterminalEntry.show()
+
+        self.expansionLabel = gtk.Label("Expansion Rate:")
+        self.expansionLabel.set_sensitive(False)
+        self.extractBox2.pack_start(self.expansionLabel, False, False, 1)
+        self.expansionLabel.show()
+
+        self.expansionEntry = gui_components.NumericEntry()
+        self.expansionEntry.set_width_chars(4)
+        self.expansionEntry.set_sensitive(False)
+        self.extractBox2.pack_start(self.expansionEntry, False, False, 0)
+        self.expansionEntry.show()
+
         self.parseDataButton = gtk.Button("Parse Data")
-        self.setInteraction.pack_start(self.parseDataButton, True, False, 1)
+        self.extractBox2.pack_end(self.parseDataButton, False, True, 1)
         self.parseDataButton.show()
 
         self.window.show()
@@ -79,6 +181,9 @@ class DataParsingGUI:
             sets[self.numSets].append(fileSelect.get_filename())
             self.parsedLabel.set_text(fileSelect.get_filename())
             self.addButton.set_sensitive(False)
+            self.dataToParse.importPreviousRun(fileSelect.get_filename())
+            self.startGenEntry.set_text(self.dataToParse.StartingGeneration)
+            self.phenotypicDiff.set_active(self.dataToParse.PhenotypicDifference)
         #elif respons == gtk.RESPONSE_CANCEL:
         fileSelect.destroy()
         
@@ -93,6 +198,22 @@ class DataParsingGUI:
         self.boxsets.append(setBox(self.numSets, self.setHBox, self.boxsets, sets[self.numSets]))
 #        self.box.addToWindow(self.setHBox)
         # self.box.show()
+
+    def parseData(self, widget, data):
+        self.dataToParse.setConfig(sets, self.parsedFileEntry.get_text(), ".", self.startGenEntry.get_text(), self.geneticDiff.get_active(), self.phenotypicDiff.get_active())
+        # this.dataToParse.parseData()
+
+    def isGenetic(self, widget, data):
+        self.lSystem.set_sensitive(not self.lSystem.get_sensitive())
+
+    def isLSystem(self, widget, data):
+        boolean = not self.terminalLabel.get_sensitive()
+        self.terminalLabel.set_sensitive(boolean)
+        self.terminalEntry.set_sensitive(boolean)
+        self.nonterminalEntry.set_sensitive(boolean)
+        self.nonterminalLabel.set_sensitive(boolean)
+        self.expansionLabel.set_sensitive(boolean)
+        self.expansionEntry.set_sensitive(boolean)
 
     def main(self):
         # All PyGTK applications must have a gtk.main(). Control ends here
@@ -210,6 +331,8 @@ class setBox:
         del self.fileList[:]
         self.setFrame.destroy()
         
+
+
 
 if __name__ == "__main__":
     hello = DataParsingGUI()
